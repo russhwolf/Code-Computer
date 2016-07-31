920f1bddf781bbee9a568f59cf22d5565bdb5db3
Russell <russhwolf@gmail.com>
2016-07-31 11:19:57 -0400

    Oscillator

---
+++ src/main/kotlin/codecomputer/Oscillator.kt
@@ -0,0 +1,57 @@
+package codecomputer
+
+/**
+ * An `Oscillator` is a [Readable] which toggles its own state back and forth. It is intended for use as a clocking input.
+ * An oscillator should not be run indefinitely, but instead provides methods to run for a specified number of cycles
+ * or until a given condition is met.
+ *
+ * This class is meant to represent the oscillator described on page 157 of *Code*, although the internal implementation
+ * is different.
+ */
+class Oscillator private constructor(private val delegate: Signal) : Readable by delegate {
+    private var active: Boolean = false
+
+    constructor(state: Boolean = false) : this(Relay(state))
+
+    /**
+     * Run this [Oscillator] until the given condition is `true`
+     */
+    fun runUntil(condition: () -> Boolean) {
+        val subscriber: (Boolean) -> Unit = {
+            if (condition()) {
+                stop()
+            }
+        }
+        subscribe(subscriber)
+        start()
+        unsubscribe(subscriber)
+    }
+
+    /**
+     * Run this [Oscillator] for the given number of cycles. The listener function, if supplied, will be called each time
+     * and passed the number of cycles which have occurred.
+     */
+    fun run(cycles: Int, listener: (Int) -> Unit = {}) {
+        var i = 0
+        runUntil {
+            val out = i >= cycles
+            if (!out) {
+                listener(i)
+                i++
+            }
+            out
+        }
+    }
+
+    private fun start(): Unit {
+        if (active) return
+        active = true
+        while (active) {
+            delegate.write(!read())
+        }
+    }
+
+    private fun stop(): Unit {
+        active = false
+    }
+}
---
+++ src/test/kotlin/codecomputer/OscillatorTest.kt
@@ -0,0 +1,37 @@
+package codecomputer
+
+import org.junit.Before
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class OscillatorTest {
+    private val CYCLES = 10
+
+    val oscillator = Oscillator()
+    private var counter = 0
+
+    @Before
+    fun setup() {
+        counter = 0
+    }
+
+    @Test fun oscillatorTest() {
+        oscillator.run(CYCLES) {
+            counter = it
+            oscillator.readAndAssert(if (it % 2 == 0) false else true, "oscillator event $it")
+        }
+
+        assertEquals(CYCLES - 1, counter, "oscillator event count")
+    }
+
+    @Test fun oscillatorTestRun1() {
+        for (i in 0 until CYCLES) {
+            oscillator.run(1) {
+                oscillator.readAndAssert(if (i % 2 == 0) false else true, "oscillator event $i")
+                counter = i
+            }
+        }
+
+        assertEquals(CYCLES - 1, counter, "oscillator event count")
+    }
+}
