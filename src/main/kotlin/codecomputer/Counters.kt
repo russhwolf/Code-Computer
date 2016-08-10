b9f12d587137efdc07a1ad5b37b114d1729bf433
Russell <russhwolf@gmail.com>
2016-08-09 22:51:24 -0400

    Ripple and ring counters

---
+++ src/main/kotlin/codecomputer/Counters.kt
@@ -0,0 +1,77 @@
+package codecomputer
+
+/**
+ * A `FrequencyDivider` changes its state half as often as the `clock` input, toggling whenever `clock` changes from
+ * `false` to `true`.
+ *
+ * This class represents the frequency divider shown on page 173 of *Code*
+ *
+ * @see RippleCounter
+ * @see AddressCounter
+ */
+class FrequencyDivider(clock: Readable, clear: Readable = Constant.FALSE, preset: Readable = Constant.FALSE,
+                       state: Boolean = true) :
+        RelayBar(state) {
+    override val bar: Readable
+
+    init {
+        val bar = Relay(!state)
+        val latch = EdgeLatch(clock, bar, clear, preset, state)
+        delegate.link(latch)
+        bar.link(latch.bar)
+        this.bar = bar
+    }
+}
+
+/**
+ * A `RippleCounter` increments the integer representation of its output when the `clock` input changes from `false` to
+ * `true`, returning to `0` when it overflows.
+ *
+ * This class represents the Ripple Counter described on page 177 of *Code*
+ *
+ * @see FrequencyDivider
+ */
+class RippleCounter(size: Int, clock: Readable, clear: Readable = Constant.FALSE, state: Int = -1) :
+        List<Readable> by ((0 until size).fold(mutableListOf<Invertible>()) { list, i ->
+            list.apply { add(FrequencyDivider(if (i == 0) !clock else list[i - 1].bar, clear, state = state.nthBit(i))) }
+        })
+
+/**
+ * An `AddressCounter` acts as a [RippleCounter] which is settable. It increments when the `clock` input changes from
+ * `false` to `true`, but also jumps directly to the value at `address` if `set` is true.
+ *
+ * This class adjusts the [RippleCounter] to incorporate the extra circuitry on page 227 of *Code*
+ *
+ * @see FrequencyDivider
+ */
+class AddressCounter(clock: Readable, address: List<Readable>, set: Readable = Constant.FALSE,
+                     reset: Readable = Constant.FALSE, state: Int = -1) :
+        List<Invertible> by ((0..address.lastIndex).fold(mutableListOf<Invertible>()) { list, i ->
+            list.apply {
+                add(FrequencyDivider(
+                        if (i == 0) !clock else list[i - 1].bar,
+                        set and !address[i] or reset,
+                        set and address[i],
+                        state.nthBit(i)
+                ))
+            }
+        })
+
+/**
+ * A `RingCounter` toggles between its outputs such that at most one is true at any given time. At construction time all
+ * outputs are false, and they will not change until the `clear` input is set to `true`. At that point, the output in the
+ * highest index will be `true` and the others `false`. Once the `clear` input is false again, the outputs will rotate
+ * every time the `clock` input changes from `true` to `false`.
+ *
+ * This class represents the ring counter described in the Page 222 section of the *Code* online technical addendum.
+ */
+class RingCounter(size: Int, clock: Readable, clear: Readable) :
+        List<Readable> by ((1..size).map { Relay() }.apply {
+            for ((i, it) in this.withIndex()) {
+                when (i) {
+                    0 -> it.link(EdgeLatch(clock, last(), clear))
+                    lastIndex -> it.link(EdgeLatch(clock, this[i - 1], preset = clear))
+                    else -> it.link(EdgeLatch(clock, this[i - 1], clear))
+                }
+            }
+        })
---
+++ src/test/kotlin/codecomputer/CounterTest.kt
@@ -0,0 +1,106 @@
+package codecomputer
+
+import org.junit.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+
+class CounterTest {
+
+    @Test fun rippleUnitTest() {
+        val clock = Relay()
+        val clear = Relay()
+        val preset = Relay()
+        val counter = FrequencyDivider(clock, clear, preset)
+        val signals = listOf(!clock, counter)
+
+        for (i in 0..20) {
+            clock.write(!clock.read())
+            signals.readAndAssert(i % 4, "ripple $i")
+        }
+        clear.write(true)
+        clear.write(false)
+        signals.readAndAssert(0, "counter clear")
+        preset.write(true)
+        preset.write(false)
+        signals.readAndAssert(2, "counter set")
+    }
+
+    @Test fun rippleCounterTest() {
+        val clock = Relay()
+        val clear = Relay()
+        val size = 4
+        val counter = RippleCounter(size, clock, clear)
+
+        assertEquals(size, counter.size, "counter size")
+        for (i in 0..20) {
+            counter.readAndAssert(i % 16, "ripple $i")
+            clock.write(!clock.read())
+            clock.write(!clock.read())
+        }
+        clear.write(true)
+        clear.write(false)
+        counter.readAndAssert(0, "counter clear")
+    }
+
+    @Test fun addressCounterTest() {
+        val clock = Relay()
+        val address = 0.toSignals(4)
+        val set = Relay()
+        val reset = Relay()
+        val counter = AddressCounter(clock, address, set, reset)
+
+        counter.readAndAssert(0, "counter 1")
+        clock.write(true)
+        clock.write(false)
+        counter.readAndAssert(1, "counter 2")
+        address.write(3)
+        counter.readAndAssert(1, "counter 3")
+        set.write(true)
+        set.write(false)
+        counter.readAndAssert(3, "counter 4")
+        clock.write(true)
+        clock.write(false)
+        counter.readAndAssert(4, "counter 5")
+        reset.write(true)
+        reset.write(false)
+        counter.readAndAssert(0, "counter 6")
+    }
+
+    @Test fun ringCounterTest() {
+        val clock = Relay()
+        val clear = Relay()
+        val size = 4
+        val counter = RingCounter(size, clock, clear)
+
+        val check = (0 until size).map { i ->
+            ((i + 1 until size).map { j ->
+                counter[i] and counter[j]
+            })
+        }.flatten().let(::MultiOr)
+        check.subscribe { assertFalse(it, "ring counter pairing checker") }
+
+        assertEquals(size, counter.size, "counter size")
+        counter.readAndAssert(0, "counter init")
+        clear.write(true)
+        clear.write(false)
+        for ((j, readable) in counter.withIndex()) {
+            readable.readAndAssert(readable == counter.last(), "counter[$j] reset")
+        }
+        for (i in 0..10) {
+            clock.write(true)
+            for ((j, readable) in counter.withIndex()) {
+                readable.readAndAssert(i % size == j, "counter[$j] $i")
+            }
+            clock.write(false)
+            for ((j, readable) in counter.withIndex()) {
+                readable.readAndAssert(i % size == j, "counter[$j] $i")
+            }
+        }
+        clear.write(true)
+        clock.write(true)
+        clear.write(false)
+        for ((j, readable) in counter.withIndex()) {
+            readable.readAndAssert(readable == counter.last(), "counter[$j] clear")
+        }
+    }
+}
